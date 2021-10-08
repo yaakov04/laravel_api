de9bd34b38f0f74c54a3b30cd8a4ab2f2ed66150
Yaakov <dbrionesmtz@yahoo.com>
2021-10-08 13:38:47 -0500

    configure el archivo del test, cree el modelo con su factoryy migraciones, cree el controlador, y configure la BD

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Http\Controllers\Api;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class PostControllerTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testExample()
     {
         $response = $this->get('/');
 
