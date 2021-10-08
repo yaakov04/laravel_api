9d48491087a1fa60adc7bf2ee89062ec556beec9
Yaakov <dbrionesmtz@yahoo.com>
2021-10-08 14:37:40 -0500

    hice ajustes en el codigo, cree validacion para el titulo

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -23,4 +23,15 @@ class PostControllerTest extends TestCase
 
         $this->assertDatabaseHas('posts', ['title'=>'El post de prueba']);
     }
+
+    public function test_validate_title()
+    {
+        $response = $this->json('POST', '/api/posts', [
+            'title'=>'',
+
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors('title');
+    }
 }
