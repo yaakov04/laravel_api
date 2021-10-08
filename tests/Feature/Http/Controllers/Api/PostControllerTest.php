21e68fa632faf29ff3d7f9c88935b0f12882db99
Yaakov <dbrionesmtz@yahoo.com>
2021-10-08 14:20:58 -0500

    configure el metodo store

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -8,15 +8,19 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+    public function test_store()
     {
-        $response = $this->get('/');
+        //$this->withoutExceptionHandling();
+        $response = $this->json('POST', '/api/posts', [
+            'title'=>'El post de prueba',
 
-        $response->assertStatus(200);
+        ]);
+
+        $response->assertJsonStructure(['id','title','created_at', 'updated_at'])
+            ->assertJson(['title'=>'El post de prueba'])
+            ->assertStatus(201);
+
+        $this->assertDatabaseHas('posts', ['title'=>'El post de prueba']);
     }
 }
